9d17dbf288422201c66fb148bdb7923cc103b6f5
myoh <myoh@nobreak.co.kr>
2021-06-09 11:02:31 +0900

    comp 2-02

--- pybo/models.py
+++ pybo/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.db.models.base import Model
+from django.db.models.deletion import CASCADE
+from django.db.models.fields import CharField
+from django.db.models.fields.related import ForeignKey
 
 # Create your models here.
+class Question(models.Model):
+    subject = models.CharField(max_length=200)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+
+    def _str_(self):
+        return self.question
