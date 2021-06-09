e1e35eb42148cf18a79df908fb8b10775aa5e960
myoh <myoh@nobreak.co.kr>
2021-06-09 15:08:11 +0900

    url

--- pybo/views.py
+++ pybo/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("Hello world")
+    """
+    pybo 목록 출력
+    """
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)
 
-# Create your views here.
+def detail(request, question_id):
+    """
+    pybo 내용 출력
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
