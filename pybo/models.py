a7117e83a0e1a776fe6540e4532260b02d60b6ff
myoh <myoh@nobreak.co.kr>
2021-06-11 14:41:27 +0900

    03-9장까지

--- pybo/models.py
+++ pybo/models.py
@@ -3,12 +3,15 @@ from django.db.models.base import Model
 from django.db.models.deletion import CASCADE
 from django.db.models.fields import CharField
 from django.db.models.fields.related import ForeignKey
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Question(models.Model):
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    modify_date = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.subject
@@ -17,6 +20,8 @@ class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    modify_date = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.content  
